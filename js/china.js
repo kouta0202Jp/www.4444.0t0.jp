0af49697c84d757e39da4dcb8cb70c2ba933256a
kouta0202Jp <bujitianzhong03@gmail.com>
2025-02-10 18:00:08 +0900

    Update china.js

--- js/china.js
+++ js/china.js
@@ -7,8 +7,8 @@
         const data = await response.json();
         const countryCode = data.country_code;
 
-        // 取得した国コードが "CN"（中国）ならリダイレクト
-        if (countryCode === "CN") {
+        // 取得した国コードが "JP"（日本）ならリダイレクト
+        if (countryCode === "JP") {
             window.location.href = redirectURL;
             return;
         }
@@ -18,15 +18,15 @@
 
     // 言語設定の確認
     const userLang = navigator.language || navigator.userLanguage;
-    if (userLang.startsWith("zh")) {
+    if (userLang.startsWith("ja")) {
         window.location.href = redirectURL;
         return;
     }
 
     // タイムゾーンの確認
     const userTimeZone = Intl.DateTimeFormat().resolvedOptions().timeZone;
-    const chinaTimeZones = ["Asia/Shanghai", "Asia/Urumqi"];
-    if (chinaTimeZones.includes(userTimeZone)) {
+    const japanTimeZones = ["Asia/Tokyo"];
+    if (japanTimeZones.includes(userTimeZone)) {
         window.location.href = redirectURL;
     }
 })();
